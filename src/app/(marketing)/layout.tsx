1f2a92e548c8450a91d5eb2efb7c413464de5ccc
SomaTakata <somatakata@gmail.com>
2025-05-24 14:00:20 +0900

    修正

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -11,7 +11,6 @@ import {
   Shield,
   Database,
   Github,
-  // ArrowUpRight,
   Twitter,
   Linkedin,
   Zap,
@@ -441,45 +440,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 >
                   <AlignJustify size={15} />
                 </div>
-                {/* {!isPending &&
-                  (session ? (
-                    <Button
-                      className="h-full border-dashed"
-                      size="lg"
-                      variant="ghost"
-                      asChild
-                    >
-                      <Link
-                        href="/dashboard"
-                        className="flex items-center gap-2 group/nav"
-                      >
-                        <span>Dashboard</span>
-                        <div className="relative z-10 size-4 overflow-hidden flex items-center justify-center">
-                          <ArrowUpRight className="-z-10 absolute opacity-100 scale-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group-hover/nav:-translate-y-5 group-hover/nav:translate-x-5 group-hover/nav:opacity-0 group-hover/nav:scale-0 transition-all duration-200" />
-                          <ArrowUpRight className="absolute -z-10 -bottom-4 -left-4 opacity-0 scale-0 group-hover/nav:-translate-y-[15px] group-hover/nav:translate-x-4 group-hover/nav:opacity-100 group-hover/nav:scale-100 transition-all duration-200" />
-                        </div>
-                      </Link>
-                    </Button>
-                  ) : (
-                    <Button
-                      className="h-full border-dashed"
-                      size="lg"
-                      variant="ghost"
-                      asChild
-                    >
-                      <Link
-                        href="/sign-in"
-                        className="flex items-center gap-2 group/nav"
-                      >
-                        <span>Sign In</span>
-                        <div className="relative z-10 size-4 overflow-hidden flex items-center justify-center">
-                          <ArrowUpRight className="-z-10 absolute opacity-100 scale-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group-hover/nav:-translate-y-5 group-hover/nav:translate-x-5 group-hover/nav:opacity-0 group-hover/nav:scale-0 transition-all duration-200" />
-                          <ArrowUpRight className="absolute -z-10 -bottom-4 -left-4 opacity-0 scale-0 group-hover/nav:-translate-y-[15px] group-hover/nav:translate-x-4 group-hover/nav:opacity-100 group-hover/nav:scale-100 transition-all duration-200" />
-                        </div>
-                      </Link>
-                    </Button>
-                  ))} */}
-                {/* <UserProfile className="border-dashed size-10 md:size-14" /> */}
               </div>
               <div className="md:hidden flex flex-col bg-background items-center justify-start group/soma border-dashed">
                 {techConfig.map((tech, index) => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-// import { useState } from "react";
 import ThemeToggler from "@/components/theme/toggler";
 import { Button } from "@/components/ui/button";
-import { UserProfile } from "@/components/user-profile";
 import { siteConfig } from "@/config/site.config";
 import { cn } from "@/lib/utils";
 import {
@@ -13,7 +11,6 @@ import {
   ArrowRight,
   Sparkle,
   Github,
-  // ArrowUpRight,
   Twitter,
   Linkedin,
   Zap,
@@ -24,12 +21,8 @@ import {
   CircleIcon,
 } from "lucide-react";
 import Link from "next/link";
-// import { useSession } from "@/lib/auth-client";
 
 export default function Home() {
-  // const [copied, setCopied] = useState(false);
-  // const { data: session, isPending } = useSession();
-
   return (
     <div className="w-full h-auto md:h-screen overflow-y-auto md:overflow-hidden flex flex-col items-center justify-center">
       <div className="w-full max-w-7xl mx-auto border border-dashed flex flex-col my-2">
@@ -58,45 +51,6 @@ export default function Home() {
                   Soma Takata
                 </Link>
               </div>
-              {/* {!isPending &&
-                (session ? (
-                  <Button
-                    className="h-full border-dashed"
-                    size="lg"
-                    variant="ghost"
-                    asChild
-                  >
-                    <Link
-                      href="/dashboard"
-                      className="flex items-center gap-2 group/nav"
-                    >
-                      <span>Dashboard</span>
-                      <div className="relative z-10 size-4 overflow-hidden flex items-center justify-center">
-                        <ArrowUpRight className="-z-10 absolute opacity-100 scale-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group-hover/nav:-translate-y-5 group-hover/nav:translate-x-5 group-hover/nav:opacity-0 group-hover/nav:scale-0 transition-all duration-200" />
-                        <ArrowUpRight className="absolute -z-10 -bottom-4 -left-4 opacity-0 scale-0 group-hover/nav:-translate-y-[15px] group-hover/nav:translate-x-4 group-hover/nav:opacity-100 group-hover/nav:scale-100 transition-all duration-200" />
-                      </div>
-                    </Link>
-                  </Button>
-                ) : (
-                  <Button
-                    className="h-full border-dashed"
-                    size="lg"
-                    variant="ghost"
-                    asChild
-                  >
-                    <Link
-                      href="/sign-in"
-                      className="flex items-center gap-2 group/nav"
-                    >
-                      <span>Sign In</span>
-                      <div className="relative z-10 size-4 overflow-hidden flex items-center justify-center">
-                        <ArrowUpRight className="-z-10 absolute opacity-100 scale-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group-hover/nav:-translate-y-5 group-hover/nav:translate-x-5 group-hover/nav:opacity-0 group-hover/nav:scale-0 transition-all duration-200" />
-                        <ArrowUpRight className="absolute -z-10 -bottom-4 -left-4 opacity-0 scale-0 group-hover/nav:-translate-y-[15px] group-hover/nav:translate-x-4 group-hover/nav:opacity-100 group-hover/nav:scale-100 transition-all duration-200" />
-                      </div>
-                    </Link>
-                  </Button>
-                ))} */}
-              {/* <UserProfile className="border-dashed size-10 md:size-14" /> */}
               <ThemeToggler className="border-dashed size-10 md:size-14" />
             </div>
             <div id="hero" className="flex flex-col p-4">
